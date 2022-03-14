b02c181c086df78b65cb0bea52fb6470def5dfc1
Samkaxe <90753108+Samkaxe@users.noreply.github.com>
2022-03-14 19:38:08 +0100

    create two pages , add admin page login , and sign up

--- src/easv/GUI/Controller/MainWindowController.java
+++ src/easv/GUI/Controller/MainWindowController.java
@@ -1,13 +1,27 @@
 package easv.GUI.Controller;
 
+import com.jfoenix.controls.JFXButton;
 import easv.BE.Event;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +32,24 @@ public class MainWindowController implements Initializable {
     public TilePane tlepane;
     @FXML
     public VBox vbox;
-
+    @FXML
+    public JFXButton signin;
+    @FXML
+    public JFXButton loginin;
+    @FXML
+    public StackPane stackpne;
+    @FXML
+    public BorderPane borderpne;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         List<Event> events = new ArrayList<>();
 
-        Event e1 = new Event("dffdf","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
-        Event e2 = new Event("dffdf","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
-        Event e3 = new Event("dffdf","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
-        Event e4 = new Event("dffdf","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
+        Event e1 = new Event("1","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
+        Event e2 = new Event("2","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
+        Event e3 = new Event("3","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
+        Event e4 = new Event("4","dfdsf","dsfsdfsdf",0 ,null ,null,"dsfsdf");
         events.add(e1);
         events.add(e2);
         events.add(e3);
@@ -38,8 +59,40 @@ public class MainWindowController implements Initializable {
            Label label = new Label(e.getName());
            vbox = new VBox();
            vbox.getChildren().add(label);
+
            tlepane.getChildren().add(vbox);
        }
     }
 
+    public void login(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/loginpage.fxml"));
+        Parent root = loader.load();
+        Scene scene = loginin.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpne.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpne.getChildren().remove(borderpne);
+        });
+        timeline.play();
+    }
+
+    public void sign(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/CreateAccount.fxml"));
+        Parent root = loader.load();
+        Scene scene = loginin.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpne.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpne.getChildren().remove(borderpne);
+        });
+        timeline.play();
+    }
 }
