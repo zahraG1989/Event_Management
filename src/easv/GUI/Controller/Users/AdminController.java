66c2c180ad464b8fca5ee27305fee20cda2b98e1
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2022-03-21 11:33:59 +0100

    Active Back btn

--- src/easv/GUI/Controller/Users/AdminController.java
+++ src/easv/GUI/Controller/Users/AdminController.java
@@ -1,10 +1,20 @@
 package easv.GUI.Controller.Users;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class AdminController {
     @FXML
@@ -18,7 +28,21 @@ public class AdminController {
     @FXML
     public TableView eventstable;
 
-    public void exitbtn(ActionEvent actionEvent) {
+    public void exitbtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/mainWindow.fxml"));
+        Parent root = loader.load();
+        Scene scene = exitid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpne.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpne.getChildren().remove(stackpne);
+        });
+        timeline.play();
+
 
     }
 
