f1c157a0dd4b9635dbb2705050937233dbcd234a
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2022-03-24 16:40:10 +0100

    .
    .

---
+++ src/easv/GUI/Controller/Users/AdminController2.java
@@ -0,0 +1,96 @@
+package easv.GUI.Controller.Users;
+
+import com.jfoenix.controls.JFXButton;
+import easv.GUI.Controller.LoginController;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class AdminController2 {
+    @FXML
+    public StackPane stackpne;
+    @FXML
+    public JFXButton exitid;
+    @FXML
+    public TableView customertable;
+    @FXML
+    public TableView eventcorditable;
+    @FXML
+    public TableView eventstable;
+
+    public JFXButton createUserid;
+
+    public LoginController cntrl ;
+
+    public void exitbtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/mainWindow.fxml"));
+        Parent root = loader.load();
+        Scene scene = exitid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpne.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpne.getChildren().remove(stackpne);
+        });
+        timeline.play();
+
+
+    }
+
+    public void deleteUserbtn(ActionEvent actionEvent) {
+
+    }
+
+    public void manageUserbtn(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteEventerbtn(ActionEvent actionEvent) {
+
+    }
+
+    public void manageEventerbtn(ActionEvent actionEvent) {
+
+    }
+
+    public void createEventbtn(ActionEvent actionEvent) {
+
+    }
+
+    public void createUserbtn(ActionEvent actionEvent) throws IOException {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/Users/adminpage.fxml"));
+        Parent root = loader.load();
+        //loader.<AdminController>getController().setController(this);
+        Scene scene = createUserid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackpne.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackpne.getChildren().remove(stackpne);
+        });
+        timeline.play();
+    }
+
+
+
+    public void setController(LoginController loginController) {
+        this.cntrl = loginController;
+    }
+}
---
+++ src/easv/GUI/Controller/Users/EventMangersController2.java
@@ -0,0 +1,212 @@
+package easv.GUI.Controller.Users;
+
+import com.jfoenix.controls.JFXButton;
+import easv.BE.Event;
+import easv.BE.Ticket;
+import easv.BE.User;
+import easv.GUI.Controller.LoginController;
+import easv.GUI.Controller.create.CreateTicketController;
+import easv.GUI.Controller.create.CreateeventController;
+import easv.GUI.Model.EventModel;
+import easv.GUI.Model.TicketModel;
+import easv.GUI.Model.UserModel;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+
+public class EventMangersController2 implements Initializable {
+    @FXML
+    public TableView<Ticket> tableviewtickets;
+    @FXML
+    public TableView<User> tableviewusers;
+    @FXML
+    public TableView<Event> tableviewevents;
+    @FXML
+    public AnchorPane ancorid;
+    @FXML
+    public StackPane stackid;
+    @FXML
+    public TableColumn<Event , String > eventname;
+    @FXML
+    public TableColumn<Event , String > eventloc;
+    @FXML
+    public TableColumn<Event , Integer> usersineventalt;
+    @FXML
+    public TableColumn<Event , Date> startevent;
+    @FXML
+    public TableColumn<Event , Date> endevent;
+    @FXML
+    public TableColumn<Event , Integer> eventticketalt;
+
+    @FXML
+    public TableColumn<User , String> username;
+    @FXML
+    public TableColumn<User , String > userticket;
+    @FXML
+    public TableColumn<User , String > usercheckbox;
+    @FXML
+    public TableColumn<Ticket , String> tickettype;
+    @FXML
+    public TableColumn<Ticket , Integer> ticketprice;
+    @FXML
+    public TableColumn<Ticket , String > ticketinfo;
+
+
+    public JFXButton createeventid;
+    public JFXButton createticketid;
+    public JFXButton deleteeventid;
+    public JFXButton deleteticketid;
+    public JFXButton updateeventid;
+    public JFXButton updateticketid;
+    public JFXButton backid;
+
+    private LoginController cntrl ;
+
+    private EventModel  eventModel ;
+    private ObservableList<Event> getallevents ;
+
+    private UserModel userModel;
+    private ObservableList<User> getAllusers;
+
+    private TicketModel ticketModel ;
+    private ObservableList<Ticket> getAllticket ;
+
+    public int selectedeventid ;
+    public Event selectedevent ;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        displayEventsintableview();
+        getuserinevent();
+        displayTicketsInsideEvent();
+
+
+
+    }
+
+    public void getUsersinSelectedEvent(MouseEvent mouseEvent) {
+        if(tableviewevents.getSelectionModel().getSelectedIndex() != -1){
+            tableviewusers.getItems().clear();
+            tableviewtickets.getItems().clear();
+            getAllusers.addAll(userModel.getuserinevent(tableviewevents.getSelectionModel().getSelectedItem().getId()));
+            getAllticket.addAll(ticketModel.getticketinevent(tableviewevents.getSelectionModel().getSelectedItem().getId()));
+            selectedeventid = tableviewevents.getSelectionModel().getSelectedItem().getId();
+             selectedevent = tableviewevents.getSelectionModel().getSelectedItem();
+            System.out.println(selectedeventid);
+        }
+
+    }
+
+
+    public void getuserinevent(){
+        userModel = UserModel.getInstance();
+        getAllusers = FXCollections.observableArrayList();
+
+        tableviewusers.setItems(getAllusers);
+
+        username.setCellValueFactory(new PropertyValueFactory<>("username"));
+        userticket.setCellValueFactory(new PropertyValueFactory<>("email"));
+        usercheckbox.setCellValueFactory(new PropertyValueFactory<>("select"));
+    }
+
+    public void displayTicketsInsideEvent(){
+        ticketModel = TicketModel.getInstance();
+        getAllticket = FXCollections.observableArrayList();
+
+        tableviewtickets.setItems(getAllticket);
+
+        tickettype.setCellValueFactory(new PropertyValueFactory<>("type"));
+        ticketinfo.setCellValueFactory(new PropertyValueFactory<>("info"));
+        ticketprice.setCellValueFactory(new PropertyValueFactory<>("ticketprice"));
+    }
+
+    public void displayEventsintableview(){
+        eventModel = EventModel.getInstance();
+        getallevents = FXCollections.observableArrayList();
+        getallevents.addAll(eventModel.getAllEvents());
+
+        tableviewevents.setItems(getallevents);
+
+        eventname.setCellValueFactory(new PropertyValueFactory<>("name"));
+        eventloc.setCellValueFactory(new PropertyValueFactory<>("location"));
+        startevent.setCellValueFactory(new PropertyValueFactory<>("startevent"));
+        endevent.setCellValueFactory(new PropertyValueFactory<>("endevent"));
+
+    }
+
+
+    public void createeventbtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/create/createevent.fxml"));
+        Parent root = loader.load();
+        loader.<CreateeventController>getController().setController(this);
+        Scene scene = createticketid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackid.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackid.getChildren().remove(ancorid);
+        });
+        timeline.play();
+    }
+
+    public void createticketbtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/easv/GUI/View/create/createTicket.fxml"));
+        Parent root = loader.load();
+        loader.<CreateTicketController>getController().setController(this);
+        Scene scene = createticketid.getScene();
+        root.translateYProperty().set(scene.getHeight());
+        stackid.getChildren().add(root);
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            stackid.getChildren().remove(ancorid);
+        });
+        timeline.play();
+    }
+
+    public void deleteeventbtn(ActionEvent actionEvent) {
+    }
+
+    public void deleteticketbtn(ActionEvent actionEvent) {
+    }
+
+    public void updateeventbtn(ActionEvent actionEvent) {
+    }
+
+    public void updateticketbtn(ActionEvent actionEvent) {
+    }
+
+    public void backbtn(ActionEvent actionEvent) {
+    }
+
+    public void setController(LoginController loginController) {
+        this.cntrl = loginController ;
+    }
+
+
+}
